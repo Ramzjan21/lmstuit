3d504dc06bb16a5a6936effadd100521bfcd2b24
jayson <bettaxacker@gmail.com>
2026-04-01 23:58:19 +0500

    Add vConsole for mobile debugging in Telegram Web App

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import VConsole from 'vconsole';
 import App from './App.jsx';
 import './index.css';
 
@@ -8,6 +9,7 @@ if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
   window.Telegram.WebApp.expand();
   window.Telegram.WebApp.setHeaderColor('#020611');
   window.Telegram.WebApp.setBackgroundColor('#020611');
+  new VConsole();
 }
 
 createRoot(document.getElementById('root')).render(
